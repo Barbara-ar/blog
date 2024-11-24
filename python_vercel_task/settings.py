91b9f732e43e1367d2c92de6e1e51f3bd53bfa37
Barbara Cabral <cabralba98@gmail.com>
2024-11-24 12:26:29 -0300

    Actualizado ALLOWED_HOSTS en settings.py para Vercel

--- python_vercel_task/settings.py
+++ python_vercel_task/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-omega-five-41.vercel.app', '127.0.0.1', 'localhost']
 
 
 # Application definition
